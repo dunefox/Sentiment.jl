76c891e6cd9fb91a0eb8cbfc29b842daf4a14cb2
Phillip Fuchs <fuchsp@cip.ifi.lmu.de>
2020-05-24 22:51:17 +0200

    asd

--- main.jl
+++ main.jl
@@ -15,6 +15,10 @@ hidden_dim = 50
 attn_dim = 30
 @info("Parameters ", classes, labels, emb_dim, hidden_dim, attn_dim)
 
+use_gpu = true # helper to easily switch between gpu/cpu
+@info("Use Gpu?", use_gpu)
+todevice(x) = use_gpu ? gpu(x) : x
+
 # model = Models.Standard(
 #     Dense(emb_dim, hidden_dim, σ),
 #     LSTM(hidden_dim, hidden_dim),
@@ -27,9 +31,9 @@ model = Models.Attention(
     LSTM(hidden_dim, hidden_dim),
     Dense(hidden_dim, attn_dim),
     Dense(hidden_dim, attn_dim),
-    rand(attn_dim),
+    rand(1, attn_dim),
     Dense(hidden_dim, length(labels))
-)
+) |> todevice
 @info("Model -> Attention", model)
 
 # path = "/home/paul/projekte/julia/jposat/glove/glove.840B.300d.txt"
@@ -43,36 +47,45 @@ loss(xᵢ, yᵢ) = -log(sum(model(xᵢ) .* Flux.onehot(yᵢ, labels)))
 ps = params(model)
 
 @info("Creating train set...")
-tr_samples = Ops.create_embeddings(train, emb_table, word_index) 
+# tr_samples = Ops.create_embeddings(train, emb_table, word_index) 
+
+tr_batches = Flux.Data.DataLoader(Ops.create_embeddings(train, emb_table, word_index, number=8000), batchsize=400, shuffle=true)
 
 Flux.testmode!(model, false)
 
 @info("Beginning training...")
-for epochᵢ in 1:6
+for epochᵢ in 1:1
     @info("Epoch $(epochᵢ) start")
-    batch_loss = 0.0
-
-    @showprogress 5 "Epoch $(epochᵢ): " for (i, (xᵢ, yᵢ)) in enumerate(tr_samples)
-        gs = gradient(ps) do
-            training_loss = loss(xᵢ, yᵢ)
-            batch_loss += training_loss
-            # reg = sum(params(model)[1] .^ 2)
-            # Insert what ever code you want here that needs Training loss, e.g. logging
-            training_loss
+    
+    @showprogress 5 "Epoch $(epochᵢ): " for batch in tr_batches
+        # batch = batch |> todevice
+        batch = todevice.(batch)
+        batch_loss = 0.0
+        for (xᵢ, yᵢ) in batch
+            gs = gradient(ps) do
+                training_loss = loss(xᵢ, yᵢ)
+                batch_loss += training_loss
+                # reg = sum(params(model)[1] .^ 2)
+                # Insert what ever code you want here that needs Training loss, e.g. logging
+                training_loss
+            end
+            # insert what ever code you want here that needs gradient
+            # E.g. logging with TensorBoardLogger.jl as histogram so you can see if it is becoming huge
+            Flux.update!(opt, ps, gs)
+            # Here you might like to check validation set accuracy, and break out to do early stopping
+            Flux.reset!(model)
         end
-        # insert what ever code you want here that needs gradient
-        # E.g. logging with TensorBoardLogger.jl as histogram so you can see if it is becoming huge
-        Flux.update!(opt, ps, gs)
-        # Here you might like to check validation set accuracy, and break out to do early stopping
-        Flux.reset!(model)
+        @info("Batch loss", batch_loss)
     end
-    @info("Epoch $(epochᵢ) end", batch_loss)
+    # @info("Epoch $(epochᵢ) end", batch_loss)
 end
 
 Flux.testmode!(model, true)
 
 @info("Creating test set...")
-te_samples = Ops.create_embeddings(test, emb_table, word_index, number=500)
+te_samples = Ops.create_embeddings(test, emb_table, word_index, number=500) |> todevice
+
+@info("Size of test data with " * number * " samples", sizeof(te_samples))
 
 @info("Calculating F₁-Score...")
 preds, gold = [], []
@@ -85,6 +98,7 @@ for (xᵢ, yᵢ) in te_samples
     push!(preds, Flux.onecold(model(xᵢ), labels))
     push!(gold, yᵢ)
 end
+# preds = preds |> cpu
+# gold = gold |> cpu
 
 @info("F₁: $(Ops.f₁(preds, gold))")
-
